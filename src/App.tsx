d4eb10bda5b84aa61ff9017da69aa2cb8364fd37
Michelle Liu <michelleliu@macbookpro.mynetworksettings.com>
2025-12-24 21:21:32 -0500

    Update portfolio content

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import { ThemeProvider } from './contexts/ThemeContext'
 import Navbar from './components/Navbar'
@@ -24,21 +24,19 @@ function ScrollToTop() {
 function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <div className="min-h-screen bg-[#FFFBF5] dark:bg-[#1a1611] transition-colors duration-300">
-          <ScrollToTop />
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/my-art" element={<MyArt />} />
-            <Route path="/my-story" element={<MyStory />} />
-            <Route path="/my-experiments" element={<MyExperiments />} />
-            <Route path="/ux-strategy" element={<UXStrategy />} />
-            <Route path="/product-catalog" element={<ProductCatalog />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
+      <div className="min-h-screen bg-[#FFFBF5] dark:bg-[#1a1611] transition-colors duration-300">
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/my-art" element={<MyArt />} />
+          <Route path="/my-story" element={<MyStory />} />
+          <Route path="/my-experiments" element={<MyExperiments />} />
+          <Route path="/ux-strategy" element={<UXStrategy />} />
+          <Route path="/product-catalog" element={<ProductCatalog />} />
+        </Routes>
+        <Footer />
+      </div>
     </ThemeProvider>
   )
 }
